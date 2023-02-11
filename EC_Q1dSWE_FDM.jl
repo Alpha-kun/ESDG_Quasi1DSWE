a05ca625714b460314deba5a32c7143f2d676986
Charlie <CharlieLiuAlpha@gmail.com>
2023-02-11 01:19:06 -0600

    FDM code changed from row major to column major

--- EC_Q1dSWE_FDM.jl
+++ EC_Q1dSWE_FDM.jl
@@ -22,8 +22,8 @@ function ψ(x)
 end
 
 function total_entropy(V)
-    AH = V[1,:]'
-    AHU = V[2,:]'
+    AH = V[:,1]
+    AHU = V[:,2]
     H = AH./A
     U = AHU./AH
     η = 0.5A.*(g.*H.^2+H.*U.^2)
@@ -31,12 +31,12 @@ function total_entropy(V)
 end
 
 #midpoint of intervals
-X=[(i-0.5)*dx for i in 1:N]'
+X=[(i-0.5)*dx for i in 1:N]
 
 #left and right boundary of intervals
 X₋=X.-0.5dx;
 X₊=X.+0.5dx;
-Xₛ=[X₋; X₊]
+Xₛ=[X₋ X₊]
 
 #the width vector
 A = a(X)
@@ -46,22 +46,22 @@ AH = ψ(X).*A
 AHU = 0*X
 
 #solution vectors
-Vec=[AH; AHU]
+Vec=[AH  AHU]
 
 #compute the central difference derivative of a row vector
 function Q(x)
-    return (circshift(x,(1,-1))-circshift(x,(1,1))) ./ (2dx)
+    return (circshift(x,-1)-circshift(x,1)) ./ (2dx)
 end
 
 #time derivatives of conserved quantities
 function dVdt(V)
-    AH = V[1,:]'
-    AHU = V[2,:]'
+    AH = V[:,1]
+    AHU = V[:,2]
     H = AH./A
     U = AHU./AH
     AH′ = -Q(AHU)
     AHU′ = -0.5*(Q(AHU.*U)+AHU.*Q(U)+U.* Q(AHU)) - g*(AH.*Q(H))
-    V′ = [AH′; AHU′];
+    V′ = [AH′ AHU′];
     return V′
 end
 
@@ -70,7 +70,7 @@ t=0
 dt = 0.001
 
 ηec=[]
-total_steps = 1000
+total_steps = 100
 
 anime = @animate for i in 1:total_steps
     #Energy Conservative scheme, rk4
@@ -82,9 +82,9 @@ anime = @animate for i in 1:total_steps
     Vec = Vec + dt*(k1+2k2+2k3+k4)/6
     push!(ηec, total_entropy(Vec))
     #plot
-    AH = Vec[1,:]
-    H = AH./(A')
-    p=plot(X', H, color="blue",ylims=(0,0.25),lab="Energy Conservative")
+    AH = Vec[:,1]
+    H = AH./(A)
+    p=plot(X, H, color="blue",ylims=(0,0.25),lab="Energy Conservative")
     display(p)
     t+=dt
 end
